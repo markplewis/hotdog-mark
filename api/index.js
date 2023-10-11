b619905aa408610be3e86c622a14c979d12db53b
Mark Plewis <markplewis1@gmail.com>
2023-10-11 18:26:16 -0400

    Replaces legacy Universal Analytics tracking ID with GA4 Google Tag ID

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-const {BetaAnalyticsDataClient} = require('@google-analytics/data');
+const { BetaAnalyticsDataClient } = require("@google-analytics/data");
 
-// https://vercel.com/docs/functions/serverless-functions/runtimes
+// Vercel Node.js runtime documentation:
 // https://vercel.com/docs/functions/serverless-functions/runtimes/node-js
-// https://developers.google.com/analytics/devguides/reporting/data/v1/quickstart-client-libraries
-// https://googleapis.dev/nodejs/analytics-data/latest/
 
-// https://googleapis.dev/nodejs/analytics-data/latest/v1beta.BetaAnalyticsDataClient.html
+// Because I can't securely deploy a Google service account key (JSON file) and assign its file path
+// to the `GOOGLE_APPLICATION_CREDENTIALS` environment variable for the API client to use, I'm
+// providing a configuration object instead.
+// - Google Analytics Data API overview: https://developers.google.com/analytics/devguides/reporting/data/v1
+// - Google Analytics Data API quickstart guide: https://developers.google.com/analytics/devguides/reporting/data/v1/quickstart-client-libraries
+// - Google Analytics Data: Node.js Client documentation: https://googleapis.dev/nodejs/analytics-data/latest/
+
+// Previously, I was using the Google Analytics Reporting API, but it's no longer supported by
+// Google Analytics 4: https://developers.google.com/analytics/devguides/reporting/core/v4
+
 const analyticsDataClient = new BetaAnalyticsDataClient({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
@@ -14,7 +21,7 @@ const analyticsDataClient = new BetaAnalyticsDataClient({
   projectId: process.env.GOOGLE_PROJECT_ID
 });
 
-const propertyId = 'UA-24981697-3';
+const propertyId = process.env.GOOGLE_ANALYTICS_PROPERTY_ID;
 
 export default async function handler(req, res) {
   try {
@@ -22,23 +29,23 @@ export default async function handler(req, res) {
       property: `properties/${propertyId}`,
       dateRanges: [
         {
-          startDate: '2018-06-21',
-          endDate: 'today',
-        },
+          startDate: "2018-06-21",
+          endDate: "today"
+        }
       ],
       dimensions: [
         {
-          name: 'city',
-        },
+          name: "city"
+        }
       ],
       metrics: [
         {
-          name: 'activeUsers',
-        },
-      ],
+          name: "activeUsers"
+        }
+      ]
     });
     res.status(200).send(response);
   } catch(error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: "An error occurred" });
   }
 }
